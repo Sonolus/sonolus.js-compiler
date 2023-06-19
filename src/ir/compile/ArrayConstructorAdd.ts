172c9dcf39078e7a3580e875d17811f1415bb3ff
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 03:10:58 +0800

    Refactored error handling in ArrayConstructorAdd

--- src/ir/compile/ArrayConstructorAdd.ts
+++ src/ir/compile/ArrayConstructorAdd.ts
@@ -1,11 +1,8 @@
-import { iterateIR } from '../iterate/index.js'
 import { ArrayConstructorAdd } from '../nodes/ArrayConstructorAdd.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileArrayConstructorAdd: CompileIR<ArrayConstructorAdd> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
-    }
+    compileIR(ir.value, ctx)
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
