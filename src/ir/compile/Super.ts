32e434ec9964ecb21deafe797ec4a71bc70595cf
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 04:46:42 +0800

    Added error messages to Super

--- src/ir/compile/Super.ts
+++ src/ir/compile/Super.ts
@@ -1,10 +1,13 @@
-import { iterateIR } from '../iterate/index.js'
 import { Super } from '../nodes/Super.js'
+import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileSuper: CompileIR<Super> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
+    const result = isConstant(ir.args)
+    if (!result) {
+        compileIR(ir.args, ctx)
+
+        throw ctx.error(ir.args, 'Arguments must be resolved at compile time')
     }
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
