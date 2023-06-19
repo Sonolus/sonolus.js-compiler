c026d1e8525d063c52010a460c5980062777e8ad
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 03:05:09 +0800

    Added non constant error messages to Member

--- src/ir/compile/Member.ts
+++ src/ir/compile/Member.ts
@@ -1,11 +1,21 @@
-import { iterateIR } from '../iterate/index.js'
 import { Member } from '../nodes/Member.js'
+import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileMember: CompileIR<Member> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
+    const objectResult = isConstant(ir.object)
+    if (!objectResult) {
+        compileIR(ir.object, ctx)
+
+        throw ctx.error(ir.object, 'Target must be resolved at compile time')
     }
 
-    throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
+    const keyResult = isConstant(ir.key)
+    if (!keyResult) {
+        compileIR(ir.key, ctx)
+
+        throw ctx.error(ir.key, 'Key must be resolved at compile time')
+    }
+
+    throw ctx.error(ir.key, 'Property does not exist')
 }
