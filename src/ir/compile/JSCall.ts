6b80ecfb6fe22514d9346f35f43aa8d0b11732cc
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 04:27:29 +0800

    Added error messages to JSCall

--- src/ir/compile/JSCall.ts
+++ src/ir/compile/JSCall.ts
@@ -1,10 +1,15 @@
-import { iterateIR } from '../iterate/index.js'
 import { JSCall } from '../nodes/JSCall.js'
+import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileJSCall: CompileIR<JSCall> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
+    for (const arg of ir.args) {
+        const result = isConstant(arg)
+        if (!result) {
+            compileIR(arg, ctx)
+
+            throw ctx.error(arg, 'Call argument must be resolved at compile time')
+        }
     }
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
