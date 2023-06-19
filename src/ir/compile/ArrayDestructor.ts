24018486aa362aa93d235e0adde1bfee759cfa6f
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-19 23:50:00 +0800

    Added iterable check to ArrayDestructor

--- src/ir/compile/ArrayDestructor.ts
+++ src/ir/compile/ArrayDestructor.ts
@@ -1,3 +1,4 @@
+import { isIterable } from '../../utils/iterable.js'
 import { iterateIR } from '../iterate/index.js'
 import { ArrayDestructor } from '../nodes/ArrayDestructor.js'
 import { isConstant } from '../optimize/transform/utils.js'
@@ -10,6 +11,8 @@ export const compileArrayDestructor: CompileIR<ArrayDestructor> = (ir, ctx) => {
 
     const result = isConstant(ir.array)
     if (!result) throw ctx.error(ir.array, 'Destructor target must be resolved at compile time')
+
+    if (!isIterable(result.value)) throw ctx.error(ir.array, 'Destructor target must be iterable')
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
--- src/ir/optimize/transform/ArrayDestructor.ts
+++ src/ir/optimize/transform/ArrayDestructor.ts
@@ -1,3 +1,4 @@
+import { isIterable } from '../../../utils/iterable.js'
 import { ArrayDestructor } from '../../nodes/ArrayDestructor.js'
 import { TransformIR } from './index.js'
 import { isConstant, transformIRAndGet } from './utils.js'
@@ -8,7 +9,9 @@ export const transformArrayDestructor: TransformIR<ArrayDestructor> = (ir, ctx) 
     const result = isConstant(array)
     if (!result) return { ...ir, array }
 
-    ir.target.elements = (result.value as unknown[]).slice()
+    if (!isIterable(result.value)) return { ...ir, array }
+
+    ir.target.elements = [...result.value]
 
     return array
 }
