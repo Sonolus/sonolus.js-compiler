e67ea66ab66fec99e2beaeab1ffb830fff1435a0
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 04:45:48 +0800

    Refactored error handling in Reference

--- src/ir/compile/Reference.ts
+++ src/ir/compile/Reference.ts
@@ -1,11 +1,6 @@
-import { iterateIR } from '../iterate/index.js'
 import { Reference } from '../nodes/Reference.js'
-import { CompileIR, compileIR } from './index.js'
+import { CompileIR } from './index.js'
 
 export const compileReference: CompileIR<Reference> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
-    }
-
-    throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
+    throw ctx.error(ir, `${ir.name} is not defined`)
 }
