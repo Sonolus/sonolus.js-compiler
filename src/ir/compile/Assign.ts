5bda9536bf74a0aaf3a392f06d365275250661bd
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 03:41:55 +0800

    Added error messages to Assign

--- src/ir/compile/Assign.ts
+++ src/ir/compile/Assign.ts
@@ -1,11 +1,21 @@
-import { iterateIR } from '../iterate/index.js'
+import { hasIntrinsicGet, hasIntrinsicSet } from '../../intrinsic/has.js'
 import { Assign } from '../nodes/Assign.js'
+import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileAssign: CompileIR<Assign> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
+    const result = isConstant(ir.lhs)
+    if (!result) {
+        compileIR(ir.lhs, ctx)
+
+        throw ctx.error(ir.lhs, 'Assignment target must be resolved at compile time')
     }
+
+    if (!hasIntrinsicSet(result)) throw ctx.error(ir.lhs, 'Assignment target must be mutable')
+
+    if (!hasIntrinsicGet(result)) throw ctx.error(ir.lhs, 'Assignment target must be readable')
+
+    compileIR(ir.rhs, ctx)
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
