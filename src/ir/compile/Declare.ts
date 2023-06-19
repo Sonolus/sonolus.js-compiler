de2b32a8d7fb52f2e7b180cd3007b215cc406a91
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 04:22:14 +0800

    Refactored error handling in Declare

--- src/ir/compile/Declare.ts
+++ src/ir/compile/Declare.ts
@@ -1,11 +1,8 @@
-import { iterateIR } from '../iterate/index.js'
 import { Declare } from '../nodes/Declare.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileDeclare: CompileIR<Declare> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
-    }
+    compileIR(ir.value, ctx)
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
