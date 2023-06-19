940c94b2fe3050391fa221ee441e03f85c3d0cff
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-19 23:57:30 +0800

    Improved iterable error message handling

--- src/ir/compile/ArrayConstructorSpread.ts
+++ src/ir/compile/ArrayConstructorSpread.ts
@@ -5,14 +5,15 @@ import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileArrayConstructorSpread: CompileIR<ArrayConstructorSpread> = (ir, ctx) => {
+    const result = isConstant(ir.arg)
+    if (result && !isIterable(result.value))
+        throw ctx.error(ir.arg, 'Spread argument must be iterable')
+
     for (const child of iterateIR(ir)) {
         compileIR(child, ctx)
     }
 
-    const result = isConstant(ir.arg)
     if (!result) throw ctx.error(ir.arg, 'Spread argument must be resolved at compile time')
-
-    if (!isIterable(result.value)) throw ctx.error(ir.arg, 'Spread argument must be iterable')
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
--- src/ir/compile/ArrayDestructor.ts
+++ src/ir/compile/ArrayDestructor.ts
@@ -5,14 +5,15 @@ import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileArrayDestructor: CompileIR<ArrayDestructor> = (ir, ctx) => {
+    const result = isConstant(ir.array)
+    if (result && !isIterable(result.value))
+        throw ctx.error(ir.array, 'Destructor target must be iterable')
+
     for (const child of iterateIR(ir)) {
         compileIR(child, ctx)
     }
 
-    const result = isConstant(ir.array)
     if (!result) throw ctx.error(ir.array, 'Destructor target must be resolved at compile time')
-
-    if (!isIterable(result.value)) throw ctx.error(ir.array, 'Destructor target must be iterable')
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
