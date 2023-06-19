702a5e649bf7aabd923841b1e8833368c6e038cb
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 04:35:19 +0800

    Added error messages to ObjectConstructorAdd

--- src/ir/compile/ObjectConstructorAdd.ts
+++ src/ir/compile/ObjectConstructorAdd.ts
@@ -1,11 +1,25 @@
-import { iterateIR } from '../iterate/index.js'
 import { ObjectConstructorAdd } from '../nodes/ObjectConstructorAdd.js'
+import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileObjectConstructorAdd: CompileIR<ObjectConstructorAdd> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
+    const keyResult = isConstant(ir.key)
+    if (!keyResult) {
+        compileIR(ir.key, ctx)
+
+        throw ctx.error(ir.key, 'Key must be resolved at compile time')
     }
+
+    if (ir.kind === 'get' || ir.kind === 'set') {
+        const valueResult = isConstant(ir.value)
+        if (!valueResult) {
+            compileIR(ir.value, ctx)
+
+            throw ctx.error(ir.value, 'Getter/setter must be resolved at compile time')
+        }
+    }
+
+    compileIR(ir.value, ctx)
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
