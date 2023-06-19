6c8c8e061deeee277517b4bff9d52e9544fb0a83
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-19 23:48:11 +0800

    Added non constant error message to ArrayDestructor

--- src/ir/compile/ArrayDestructor.ts
+++ src/ir/compile/ArrayDestructor.ts
@@ -1,11 +1,15 @@
 import { iterateIR } from '../iterate/index.js'
 import { ArrayDestructor } from '../nodes/ArrayDestructor.js'
+import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileArrayDestructor: CompileIR<ArrayDestructor> = (ir, ctx) => {
     for (const child of iterateIR(ir)) {
         compileIR(child, ctx)
     }
+
+    const result = isConstant(ir.array)
+    if (!result) throw ctx.error(ir.array, 'Destructor target must be resolved at compile time')
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
