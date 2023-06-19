4ffdd2f5cee995f23478e7ba671c63ecfdd54668
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 04:01:59 +0800

    Added error messages to Call

--- src/ir/compile/Call.ts
+++ src/ir/compile/Call.ts
@@ -1,11 +1,24 @@
-import { iterateIR } from '../iterate/index.js'
 import { Call } from '../nodes/Call.js'
+import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileCall: CompileIR<Call> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
+    const calleeResult = isConstant(ir.callee)
+    if (!calleeResult) {
+        compileIR(ir.callee, ctx)
+
+        throw ctx.error(ir.callee, 'Callee must be resolved at compile time')
     }
+
+    const argsResult = isConstant(ir.args)
+    if (!argsResult) {
+        compileIR(ir.args, ctx)
+
+        throw ctx.error(ir.args, 'Call arguments must be resolved at compile time')
+    }
+
+    if (typeof calleeResult.value !== 'function')
+        throw ctx.error(ir.callee, 'Callee is not a function')
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
