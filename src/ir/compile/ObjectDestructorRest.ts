7b525228ba4d6f0ea3a367e7de751cbab80dbba1
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 04:44:35 +0800

    Refactored error handling in ObjectDestructorRest

--- src/ir/compile/ObjectDestructorRest.ts
+++ src/ir/compile/ObjectDestructorRest.ts
@@ -1,11 +1,6 @@
-import { iterateIR } from '../iterate/index.js'
 import { ObjectDestructorRest } from '../nodes/ObjectDestructorRest.js'
-import { CompileIR, compileIR } from './index.js'
+import { CompileIR } from './index.js'
 
 export const compileObjectDestructorRest: CompileIR<ObjectDestructorRest> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
-    }
-
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
