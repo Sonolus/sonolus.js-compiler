405676ffbd783add6c9dd675b7ec666b16befa97
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 04:44:06 +0800

    Added error messages to ObjectDestructorGet

--- src/ir/compile/ObjectDestructorGet.ts
+++ src/ir/compile/ObjectDestructorGet.ts
@@ -1,10 +1,13 @@
-import { iterateIR } from '../iterate/index.js'
 import { ObjectDestructorGet } from '../nodes/ObjectDestructorGet.js'
+import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileObjectDestructorGet: CompileIR<ObjectDestructorGet> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
+    const result = isConstant(ir.key)
+    if (!result) {
+        compileIR(ir.key, ctx)
+
+        throw ctx.error(ir.key, 'Key must be resolved at compile time')
     }
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
