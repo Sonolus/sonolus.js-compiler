79b43abd8244d4130fd49c64a44ac2bf6503c428
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 04:31:56 +0800

    Added error messages to New

--- src/ir/compile/New.ts
+++ src/ir/compile/New.ts
@@ -1,11 +1,24 @@
-import { iterateIR } from '../iterate/index.js'
 import { New } from '../nodes/New.js'
+import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileNew: CompileIR<New> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
+    const calleeResult = isConstant(ir.callee)
+    if (!calleeResult) {
+        compileIR(ir.callee, ctx)
+
+        throw ctx.error(ir.callee, 'Callee must be resolved at compile time')
     }
+
+    const argsResult = isConstant(ir.args)
+    if (!argsResult) {
+        compileIR(ir.args, ctx)
+
+        throw ctx.error(ir.args, 'New arguments must be resolved at compile time')
+    }
+
+    if (typeof calleeResult.value !== 'function')
+        throw ctx.error(ir.callee, 'Callee is not a function')
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
