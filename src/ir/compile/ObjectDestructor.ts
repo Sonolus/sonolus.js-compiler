760ec9e40e285342958591d55b1ee732bc446341
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 04:41:01 +0800

    Added error messages to ObjectDestructor

--- src/ir/compile/ObjectDestructor.ts
+++ src/ir/compile/ObjectDestructor.ts
@@ -1,11 +1,18 @@
-import { iterateIR } from '../iterate/index.js'
+import { isSpreadable } from '../../utils/spreadable.js'
 import { ObjectDestructor } from '../nodes/ObjectDestructor.js'
+import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileObjectDestructor: CompileIR<ObjectDestructor> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
+    const result = isConstant(ir.object)
+    if (!result) {
+        compileIR(ir.object, ctx)
+
+        throw ctx.error(ir.object, 'Destructor target must be resolved at compile time')
     }
+
+    if (!isSpreadable(result.value))
+        throw ctx.error(ir.object, 'Destructor target must be spreadable')
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
