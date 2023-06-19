15f5ad2509f7a1d20b64e9b1f6878d50242eb7e4
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-19 23:40:03 +0800

    Added non constant error message to ArrayConstructorSpread

--- src/ir/compile/ArrayConstructorSpread.ts
+++ src/ir/compile/ArrayConstructorSpread.ts
@@ -1,11 +1,15 @@
 import { iterateIR } from '../iterate/index.js'
 import { ArrayConstructorSpread } from '../nodes/ArrayConstructorSpread.js'
+import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileArrayConstructorSpread: CompileIR<ArrayConstructorSpread> = (ir, ctx) => {
     for (const child of iterateIR(ir)) {
         compileIR(child, ctx)
     }
+
+    if (!isConstant(ir.arg))
+        throw ctx.error(ir.arg, 'Spread argument must be resolved at compile time')
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
