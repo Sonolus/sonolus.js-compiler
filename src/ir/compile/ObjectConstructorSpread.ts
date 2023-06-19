bd65fb94b1670dc391bd37c3758160df34393927
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 04:38:49 +0800

    Added error messages to ObjectConstructorSpread

--- src/ir/compile/ObjectConstructorSpread.ts
+++ src/ir/compile/ObjectConstructorSpread.ts
@@ -1,11 +1,17 @@
-import { iterateIR } from '../iterate/index.js'
+import { isSpreadable } from '../../utils/spreadable.js'
 import { ObjectConstructorSpread } from '../nodes/ObjectConstructorSpread.js'
+import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileObjectConstructorSpread: CompileIR<ObjectConstructorSpread> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
+    const result = isConstant(ir.arg)
+    if (!result) {
+        compileIR(ir.arg, ctx)
+
+        throw ctx.error(ir.arg, 'Spread argument must be resolved at compile time')
     }
+
+    if (!isSpreadable(result.value)) throw ctx.error(ir.arg, 'Spread argument must be spreadable')
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
