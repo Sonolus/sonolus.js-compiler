60023fa04d994cc784bf517ef9353ee3ed0a464e
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 03:26:25 +0800

    Refactored error handling in ArrayDestructorGet

--- src/ir/compile/ArrayDestructorGet.ts
+++ src/ir/compile/ArrayDestructorGet.ts
@@ -1,11 +1,6 @@
-import { iterateIR } from '../iterate/index.js'
 import { ArrayDestructorGet } from '../nodes/ArrayDestructorGet.js'
-import { CompileIR, compileIR } from './index.js'
+import { CompileIR } from './index.js'
 
 export const compileArrayDestructorGet: CompileIR<ArrayDestructorGet> = (ir, ctx) => {
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
-    }
-
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
