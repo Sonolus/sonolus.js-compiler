724d171959f60fd25241027577166256a93fcf03
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 03:16:36 +0800

    Refactored error handling in ArrayConstructorSpread

--- src/ir/compile/ArrayConstructorSpread.ts
+++ src/ir/compile/ArrayConstructorSpread.ts
@@ -1,19 +1,17 @@
 import { isIterable } from '../../utils/iterable.js'
-import { iterateIR } from '../iterate/index.js'
 import { ArrayConstructorSpread } from '../nodes/ArrayConstructorSpread.js'
 import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileArrayConstructorSpread: CompileIR<ArrayConstructorSpread> = (ir, ctx) => {
     const result = isConstant(ir.arg)
-    if (result && !isIterable(result.value))
-        throw ctx.error(ir.arg, 'Spread argument must be iterable')
+    if (!result) {
+        compileIR(ir.arg, ctx)
 
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
+        throw ctx.error(ir.arg, 'Spread argument must be resolved at compile time')
     }
 
-    if (!result) throw ctx.error(ir.arg, 'Spread argument must be resolved at compile time')
+    if (!isIterable(result.value)) throw ctx.error(ir.arg, 'Spread argument must be iterable')
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
