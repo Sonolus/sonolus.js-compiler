a968b5ed01cbe0722dcb79e146ca0ba89845f78a
NonSpicyBurrito <burritoyangster@gmail.com>
2023-06-20 03:22:53 +0800

    Refactored error handling in ArrayDestructor

--- src/ir/compile/ArrayDestructor.ts
+++ src/ir/compile/ArrayDestructor.ts
@@ -1,19 +1,17 @@
 import { isIterable } from '../../utils/iterable.js'
-import { iterateIR } from '../iterate/index.js'
 import { ArrayDestructor } from '../nodes/ArrayDestructor.js'
 import { isConstant } from '../optimize/transform/utils.js'
 import { CompileIR, compileIR } from './index.js'
 
 export const compileArrayDestructor: CompileIR<ArrayDestructor> = (ir, ctx) => {
     const result = isConstant(ir.array)
-    if (result && !isIterable(result.value))
-        throw ctx.error(ir.array, 'Destructor target must be iterable')
+    if (!result) {
+        compileIR(ir.array, ctx)
 
-    for (const child of iterateIR(ir)) {
-        compileIR(child, ctx)
+        throw ctx.error(ir.array, 'Destructor target must be resolved at compile time')
     }
 
-    if (!result) throw ctx.error(ir.array, 'Destructor target must be resolved at compile time')
+    if (!isIterable(result.value)) throw ctx.error(ir.array, 'Destructor target must be iterable')
 
     throw ctx.error(ir, `${ir.type} must be resolved at compile time`)
 }
