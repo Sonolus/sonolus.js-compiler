de7be15c0f415b654514d5d6dba8d5bfbfb05b5d
NonSpicyBurrito <burritoyangster@gmail.com>
2023-08-19 14:45:56 +0800

    Added optimizing zero and one case Switch

--- src/ir/optimize/transform/Switch.ts
+++ src/ir/optimize/transform/Switch.ts
@@ -1,14 +1,32 @@
 import { Switch } from '../../nodes/Switch.js'
 import { TransformIR, transformIR } from './index.js'
-import { transformIRAndGet } from './utils.js'
+import { rewriteAsExecute, transformIRAndGet } from './utils.js'
 
 export const transformSwitch: TransformIR<Switch> = (ir, ctx) => {
     const discriminant = transformIRAndGet(ir.discriminant, ctx)
-    const defaultCase = transformIR(ir.defaultCase, ctx)
+    const defaultCase = transformIRAndGet(ir.defaultCase, ctx)
     const cases = ir.cases.map(({ test, consequent }) => ({
         test: transformIRAndGet(test, ctx),
-        consequent: transformIR(consequent, ctx),
+        consequent: transformIRAndGet(consequent, ctx),
     }))
 
-    return { ...ir, discriminant, defaultCase, cases }
+    switch (cases.length) {
+        case 0:
+            return rewriteAsExecute(ir, ctx, [discriminant, defaultCase])
+        case 1:
+            return transformIR(
+                ctx.Conditional(ir, {
+                    test: ctx.Binary(ir, {
+                        operator: '===',
+                        lhs: discriminant,
+                        rhs: cases[0].test,
+                    }),
+                    consequent: cases[0].consequent,
+                    alternate: defaultCase,
+                }),
+                ctx,
+            )
+        default:
+            return { ...ir, discriminant, defaultCase, cases }
+    }
 }
