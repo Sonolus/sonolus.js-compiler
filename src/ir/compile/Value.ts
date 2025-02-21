1118340d236a6f7ac158463df98b9ab49e0d08c6
Burrito <burritoyangster@gmail.com>
2025-02-21 18:40:18 +0800

    Add NaN protection (#31)

--- src/ir/compile/Value.ts
+++ src/ir/compile/Value.ts
@@ -4,8 +4,13 @@ import { CompileIR } from './index.js'
 export const compileValue: CompileIR<Value> = (ir, ctx) => {
     switch (typeof ir.value) {
         case 'number':
-        case 'boolean':
-            return +ir.value
+        case 'boolean': {
+            const value = +ir.value
+            if (Number.isNaN(value))
+                throw ctx.error(ir, `Conversion of value ${ir.value} results in NaN`)
+
+            return value
+        }
         default:
             throw ctx.error(ir, `Value of ${typeof ir.value} type must be resolved at compile time`)
     }
